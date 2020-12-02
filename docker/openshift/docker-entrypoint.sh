1f67ffdd51bbda7bba761b9511d6aa361e38b41f
Iivo Raitahila <iivo.raitahila@ibm.com>
2020-12-02 12:21:55 +0200

    drush configuration

--- docker/openshift/docker-entrypoint.sh
+++ docker/openshift/docker-entrypoint.sh
@@ -5,16 +5,21 @@ echo "Copy Drupal to the root folder of the web server"
 rsync -vah "/opt/drupal/public/" "/var/www/html/public/" --exclude="web/sites/default/files"
 rsync -vah "/opt/drupal/vendor/" "/var/www/html/vendor/"
 rsync -vah "/opt/drupal/conf/" "/var/www/html/conf/"
+cp /opt/drupal/composer.* /var/www/html/
+# copy /opt/drupal/drush (and other files & folders) to /var/www/html as they become necessary
 
 echo "Set cron key"
 if [[ ! -z "${DRUPAL_CRON_KEY}" ]]; then
     echo ${DRUPAL_CRON_KEY}
-    ./vendor/bin/drush sset system.cron_key ${DRUPAL_CRON_KEY}
+    drush sset system.cron_key ${DRUPAL_CRON_KEY}
 fi
-
-echo "Run PHP-FPM in the background as a daemon"
-php-fpm
 
 echo "To install Drupal for the first time (clears database), execute"
 echo "drush site:install --existing-config"
 echo "in /var/www/html/public via the terminal"
+cd /var/www/html/public && drush cr
+
+echo "Run PHP-FPM in the background as a daemon"
+php-fpm
+
+
