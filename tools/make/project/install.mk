7c9bc65ccdb124c78a8b73b60ff453f3236a23af
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2021-04-15 14:21:56 +0300

    UHF-1336: Updated tools.

--- tools/make/project/install.mk
+++ tools/make/project/install.mk
@@ -1,6 +1,6 @@
-ifeq ($(DRUPAL_CONF_EXISTS)$(DRUPAL_VERSION),yes8)
-    DRUPAL_NEW_TARGETS := up build drush-si drush-enable-modules drush-locale-import drush-uli
-    DRUPAL_POST_INSTALL_TARGETS := drush-updb drush-cim drush-locale-import drush-uli
+ifeq ($(DRUPAL_VERSION),8)
+    DRUPAL_NEW_TARGETS := up build drush-si drush-enable-modules drush-locale-update drush-uli
+    DRUPAL_POST_INSTALL_TARGETS := drush-updb drush-cim drush-locale-update drush-uli
 endif
 
 PHONY += drush-enable-modules
@@ -8,7 +8,8 @@ drush-enable-modules: ## Enable modules and base configurations.
 	$(call step,Install base configurations...)
 	$(call drush_on_docker,en -y helfi_platform_config helfi_base_config)
 
-PHONY += drush-locale-import
-drush-locale-import: ## Import locale PO files
-	$(call step,Import locale PO files...)
-	$(call drush_on_docker,helfi:locale-import helfi_platform_config)
+PHONY += drush-locale-update
+drush-locale-update: ## Update translations.
+	$(call step,Update translations...)
+	$(call drush_on_docker,state:set locale.translation_last_checked 0)
+	$(call drush_on_docker,locale:update)
