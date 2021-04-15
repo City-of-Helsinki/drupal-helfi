4412ef2ab3a2834ee21f82ba9c5f2158e030f53b
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2021-04-15 15:09:55 +0300

    UHF-1336: Updated locale:export drush command.

--- tools/make/project/locale.mk
+++ tools/make/project/locale.mk
@@ -4,9 +4,15 @@ else
 	PATH_TO_PLATFORM_CONFIG := ./public/modules/custom/helfi_platform_config/translations
 endif
 
+comma:=,
+
 PHONY += drush-locale-export
 drush-locale-export: ## Export locale PO files
 	$(call step,Export locale PO files...)
-	$(call drush_on_${RUN_ON},locale-export fi > ${PATH_TO_PLATFORM_CONFIG}/fi/strings.po)
-	$(call drush_on_${RUN_ON},locale-export sv > ${PATH_TO_PLATFORM_CONFIG}/sv/strings.po)
-	$(call drush_on_${RUN_ON},locale-export ru > ${PATH_TO_PLATFORM_CONFIG}/ru/strings.po)
+	$(call step,Handle Finnish...)
+	$(call drush_on_${RUN_ON},locale:export fi --types=customized${comma}not-translated > ${PATH_TO_PLATFORM_CONFIG}/fi/strings.po)
+	$(call step,Handle Swedish...)
+	$(call drush_on_${RUN_ON},locale:export sv --types=customized${comma}not-translated > ${PATH_TO_PLATFORM_CONFIG}/sv/strings.po)
+	$(call step,Handle Russian...)
+	$(call drush_on_${RUN_ON},locale:export ru --types=customized${comma}not-translated > ${PATH_TO_PLATFORM_CONFIG}/ru/strings.po)
+	$(call step,All files exported to ${PATH_TO_PLATFORM_CONFIG})
