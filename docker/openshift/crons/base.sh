39232f5222b34000ab72f1842f74cd95c2cf7ca9
TautvydasGustas1 <39690430+TautvydasGustas1@users.noreply.github.com>
2021-09-07 10:20:43 +0300

    Update base.sh

--- docker/openshift/crons/base.sh
+++ docker/openshift/crons/base.sh
@@ -1,1 +1,25 @@
+echo "Starting cron: $(date)"
 
+# You can add any additional cron "daemons" here:
+#
+# exec "/crons/some-command.sh" &
+#
+# Example cron (docker/openshift/crons/some-command.sh):
+# @code
+# #!/bin/bash
+# while true
+# do
+#   drush some-command
+#   sleep 600
+# done
+# @endcode
+
+exec "/crons/migrate-tpr.sh" &
+
+while true
+do
+  echo "Running cron: $(date)\n"
+  drush cron
+  # Sleep for 10 minutes.
+  sleep 600
+done
