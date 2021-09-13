6eec19341b09c83b2b758afbe650e4282a124e47
tuutti <tuutti@iki.fi>
2021-09-13 14:27:27 +0300

    UHF-2396:

--- docker/openshift/entrypoints/20-deploy.sh
+++ docker/openshift/entrypoints/20-deploy.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-cd /var/www/html/public && drush deploy
+cd /var/www/html/public
+
+# Make sure we have active Drupal configuration.
+if [ ! -f "../conf/cmi/system.site.yml" ]; then
+  echo "Codebase is not deployed properly."
+  exit 1
+fi
+
+VALUE=$(drush state:get last_deploy)
+
+# This script is run every time a container is spawned and certain environments might
+# start more than one Drupal container and this is used to make sure we run deploy
+# tasks only once per deploy.
+# Test it's been at least 5 minutes since last deploy.
+if [ ! -n "$VALUE" ] || [[ ($(date +%s) > $(expr "$VALUE" + 300)) ]]; then
+  drush state:set last_deploy $(date +%s)
+  drush deploy
+  exit 0
+fi
